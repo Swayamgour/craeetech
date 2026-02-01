ff3aa5ec126565275bbfd29766936913a6eaf15c
Swayam Gaur <goura0775@gmail.com>
2026-02-01 23:37:46 +0530

    add api

--- src/config.jsx
+++ src/config.jsx
@@ -1,5 +1,6 @@
 const config = {
-    backendBaseUrl: "http://localhost:5001"
+    // backendBaseUrl: "http://localhost:5001"
+    backendBaseUrl: "https://backend-createch.onrender.com"
 };
 
 export default config;
